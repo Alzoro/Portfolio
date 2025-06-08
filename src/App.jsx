cbab89c954cc315388010ad4d1fd5b30553a5c0c
Alzoro <jones.200423@gmail.com>
2025-06-08 22:27:23 +0530

    completed skills section fully

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,32 @@ import exe from './images/exe.png';
 import instagram from './images/instagram.png';
 import github from './images/github.png';
 import linkedin from './images/linkedin.png';
+import c from './images/Skills/c.png';
+import python from './images/Skills/python.png';
+import mysql from './images/Skills/mysql.png';
+import latex from './images/Skills/latex.png';
+import makefile from './images/Skills/makefile.png';
+import bash from './images/Skills/bash.png';
+import nmap from './images/Skills/nmap.png';
+import burp from './images/Skills/burp.png';
+import bettercap from './images/Skills/bettercap.png';
+import aircrack from './images/Skills/aircrack.png';
+import john from './images/Skills/john.png';
+import hydra from './images/Skills/hydra.png';
+import sqlmap from './images/Skills/sqlmap.png';
+import hashcat from './images/Skills/hashcat.png';
+import splunk from './images/Skills/splunk.png';
+import autopsy from './images/Skills/autopsy.png';
+import canva from './images/Skills/canva.png';
+import figma from './images/Skills/figma.png';
+import css from './images/Skills/css.png';
+import html from './images/Skills/html.png';
+import js from './images/Skills/js.png';
+import react from './images/Skills/react.png';
+import et from './images/Skills/et.png';
+import net from './images/Skills/net.png';
+import linux from './images/Skills/linux.png';
+import pen from './images/Skills/pen.png';
 
 
 function Background() {
@@ -180,7 +206,7 @@ function Background() {
             <p className='content-title'>Skills.txt</p>
 
           </div>
-          <p className='aaa'>This is the dev content asd asd asda asd asd asd asd asd  opened by clicking the image.</p>
+          <Skill />
         </div>
         )}
 
@@ -193,7 +219,7 @@ function Background() {
             <p className='content-title'>Experience.txt</p>
 
           </div>
-          <p className='aaa'>This is the dev content asd asd asda asd asd asd asd asd  opened by clicking the image.</p>
+            <Experience />
         </div>
         )}
 
@@ -280,19 +306,19 @@ function Background() {
 
           <div className='ffile-files'>
             <div className='insta'>
-              <img className='instagram' src={instagram} alt='text file'  
+              <img className='instagram' src={instagram} alt='instagram'  
               onClick={()=> window.open('https://www.instagram.com/wannabejnz._?igsh=N3gyamZvdWFvb2Vt','_blank')}/>
               <p className='aabout_caption'>Instagram</p>
             </div>
 
             <div className='linked'>
-              <img className='linkedin' src={linkedin} alt='text file'  
+              <img className='linkedin' src={linkedin} alt='linkedin'  
               onClick={()=> window.open('https://www.linkedin.com/in/jones24','_blank')}/>
               <p className='aabout_caption'>LinkedIn</p>
             </div>
 
             <div className='git'>
-              <img className='github' src={github} alt='text file'  
+              <img className='github' src={github} alt='github'  
               onClick={()=> window.open('https://github.com/Alzoro','_blank')}/>
               <p className='aabout_caption'>GitHub</p>
             </div>
@@ -453,7 +479,9 @@ function Background() {
             <p className='content-title'>Skills.txt</p>
 
           </div>
-          <p className='aaa'>This is the dev content asd asd asda asd asd asd asd asd  opened by clicking the image.</p>
+          <div>
+            <Skill />
+          </div>
         </div>
         )}
 
@@ -640,6 +668,265 @@ function Background() {
 
       </div>
     </>
+  );
+}
+
+
+function Skill(){
+  return(
+    <div className='skills-con'>
+            <p>
+              I not only work with these technologies but excellent in using them with best practices to 
+              deliver highh-quality results, I have veen working with all these skills to build my 
+              portfolio projects
+            </p>
+            <p className='p1'>Programming	Languages</p>
+            <div className='skills1'>
+              <div className='s1'>
+                <img className='c' src={c} alt='c'/>
+                <p className='aabout_caption'>C</p>
+              </div>
+
+              <div className='s2'>
+                <img className='python' src={python} alt='python'/>
+                <p className='aabout_caption'>Python</p>
+              </div>
+
+              <div className='s3'>
+                <img className='bash' src={bash} alt='bash'/>
+                <p className='aabout_caption'>Bash Script</p>
+              </div>
+
+              <div className='s4'>
+                <img className='sql' src={mysql} alt='sql'/>
+                <p className='aabout_caption'>My SQL</p>
+              </div>
+
+              <div className='s5'>
+                <img className='makefile' src={makefile} alt='makefile'/>
+                <p className='aabout_caption'>MakeFile</p>
+              </div>
+
+              <div className='s6'>
+                <img className='latex' src={latex} alt='latex'/>
+                <p className='aabout_caption'>LaTeX</p>
+              </div>
+            </div>
+            <p className='p2'>Tools	and	Technologies</p>
+            
+            <div className='skills2'>
+              <div className='s7'>
+                <img className='nmap' src={nmap} alt='nmap'/>
+                <p className='aabout_caption'>Nmap</p>
+              </div>
+
+              <div className='s8'>
+                <img className='burp' src={burp} alt='burp'/>
+                <p className='aabout_caption'>Burp Suite</p>
+              </div>
+
+              <div className='s9'>
+                <img className='meta' src={img5} alt='metasploit'/>
+                <p className='aabout_caption'>Metasploit</p>
+              </div>
+
+              <div className='s10'>
+                <img className='wireshark' src={img6} alt='wireshark'/>
+                <p className='aabout_caption'>Wireshark</p>
+              </div>
+
+              <div className='s11'>
+                <img className='bettercap' src={bettercap} alt='bettercap'/>
+                <p className='aabout_caption'>Bettercap</p>
+              </div>
+
+              <div className='s12'>
+                <img className='air' src={aircrack} alt='air'/>
+                <p className='aabout_caption'>Aircrack-ng</p>
+              </div>
+
+              <div className='s13'>
+                <img className='john' src={john} alt='john'/>
+                <p className='aabout_caption'>John the Ripper</p>
+              </div>
+
+              <div className='s14'>
+                <img className='hydra' src={hydra} alt='hydra'/>
+                <p className='aabout_caption'>Hydra</p>
+              </div>
+
+              <div className='s15'>
+                <img className='sqlmap' src={sqlmap} alt='sqlmap'/>
+                <p className='aabout_caption'>sqlmap</p>
+              </div>
+
+              <div className='s16'>
+                <img className='hashcat' src={hashcat} alt='hashcat'/>
+                <p className='aabout_caption'>Hashcat</p>
+              </div>
+
+              <div className='s17'>
+                <img className='splunk' src={splunk} alt='splunk'/>
+                <p className='aabout_caption'>Splunk</p>
+              </div>
+
+              <div className='s18'>
+                <img className='autopsy' src={autopsy} alt='autopsy'/>
+                <p className='aabout_caption'>Autopsy</p>
+              </div>
+
+              <div className='s19'>
+                <img className='canva' src={canva} alt='canva'/>
+                <p className='aabout_caption'>Canva</p>
+              </div>
+
+              <div className='s20'>
+                <img className='figma' src={figma} alt='figma'/>
+                <p className='aabout_caption'>Figma</p>
+              </div>
+
+              <div className='s21'>
+                <img className='git' src={github} alt='git'/>
+                <p className='aabout_caption'>GitHub</p>
+              </div>
+            </div>
+            <p className='p3'>Web Development</p>
+            <div className='skills3'>
+              <div className='s22'>
+                <img className='css' src={css} alt='css'/>
+                <p className='aabout_caption'>CSS</p>
+              </div>
+
+              <div className='s23'>
+                <img className='html' src={html} alt='html'/>
+                <p className='aabout_caption'>HTML</p>
+              </div>
+
+              <div className='s24'>
+                <img className='js' src={js} alt='js'/>
+                <p className='aabout_caption'>JavaScript</p>
+              </div>
+
+              <div className='s25'>
+                <img className='react' src={react} alt='react'/>
+                <p className='aabout_caption'>React</p>
+              </div>
+            </div>
+            <p className='p4'>Technical</p>
+            <div className='skills4'>
+              <div className='s26'>
+                <img className='et' src={et} alt='et'/>
+                <p className='aabout_caption'>Ethical Hacking</p>
+              </div>
+
+              <div className='s27'>
+                <img className='net' src={net} alt='net'/>
+                <p className='aabout_caption'>Networking</p>
+              </div>
+
+              <div className='s28'>
+                <img className='linux' src={linux} alt='linux'/>
+                <p className='aabout_caption'>Linux</p>
+              </div>
+
+              <div className='s29'>
+                <img className='pen' src={pen} alt='pen'/>
+                <p className='aabout_caption'>Penetration Testing</p>
+              </div>
+            </div>
+            <p className='bottom'>&nbsp;</p>
+
+            
+          </div>
+  );
+}
+
+function Experience() {
+  return (
+
+  <div class="exp-con">
+    <div class="experience-row">
+      <div class="experience-label">Company:</div>
+      <div>NTC	Logistics	India	[P]	Limited</div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Duration:</div>
+      <div>October 2024 - October 2024</div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Role:</div>
+      <div>Security Auditor Intern</div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Experience:</div>
+      <div className='list'>
+        <ul>
+          <li>Contributed to securing NTC's systems by identifying and patching vulnerabilities during a Security Auditor internship.</li>
+          <li>Analyzed firewall technologies and rules, improving network security understanding.</li>
+          <li>Strengthened threat mitigation strategies by applying practical cybersecurity principles.</li>
+        </ul>
+      </div>
+    </div>
+
+    <hr className='line1'/>
+
+    <div className='space1'></div>
+
+    <div class="experience-row">
+      <div class="experience-label">Company:</div>
+      <div> Internship	Studio</div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Duration:</div>
+      <div>August 2024 - September 2024</div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Role:</div>
+      <div>Intern</div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Experience:</div>
+      <div className='list'>
+        <ul>
+          <li> Worked with advanced cybersecurity tools, enhancing skills in identifying and mitigating system
+            vulnerabilities.</li>
+          <li>Collaborated with a supportive team, receiving mentorship to refine technical expertise and practical
+            application of cybersecurity concepts.</li>
+        </ul>
+      </div>
+    </div>
+
+    <hr className='line2'/>
+
+     <div className='space2'></div>
+
+    <div class="experience-row">
+      <div class="experience-label">Company:</div>
+      <div> Tata	Cybersecurity	Security	Analyst	Job	Simulation	on	Forage</div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Duration:</div>
+      <div>January 2025 </div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Role:</div>
+      <div>Intern</div>
+    </div>
+    <div class="experience-row">
+      <div class="experience-label">Experience:</div>
+      <div className='list'>
+        <ul>
+          <li>  Completed a job simulation involving identity and access management (IAM) for Tata Consultancy Services,
+            collaborating with a Cybersecurity Consulting team.</li>
+          <li> Acquired expertise in IAM principles, cybersecurity best practices, and strategic alignment with business
+            objectives.</li>
+            <li>Delivered comprehensive documentation and presentations, showcasing the ability to communicate
+              complex technical concepts effectively.</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
   );
 }
 
